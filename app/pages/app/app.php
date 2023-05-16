bfad9b2b4137e233fb9bbcb8dc60424c8039f884
AndrewTalanov <andrewtalanov@mail.ru>
2023-05-16 17:49:41 +0500

    микро изменения верстки

--- app/pages/app/app.php
+++ app/pages/app/app.php
@@ -14,48 +14,8 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php require_once '../../includes/css-bootstrap.php' ?>
   <script type="module" src="../../js/checkAuth.js"></script>
-  <title>ПРИЛОЖЕНИЕ</title>
-  <style>
-    .container-fluid {
-      padding: 0;
-    }
-
-    .row {
-      margin: 0;
-    }
-
-    .main-section {
-      background-color: #f5f5f5;
-      min-height: 100vh;
-    }
-
-    .nav-menu {
-      background-color: #333;
-      color: #fff;
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-    }
-
-    .nav-menu ul {
-      padding: 0;
-      list-style: none;
-    }
-
-    .nav-menu li {
-      padding: 10px;
-    }
-
-    .nav-menu li:hover {
-      background-color: #555;
-      cursor: pointer;
-    }
-
-    .logout-btn {
-      margin-bottom: 20px;
-    }
-  </style>
+  <title>Главная</title>
+  <link rel="stylesheet" href="./app.css">
 </head>
 
 <body>
